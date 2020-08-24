68a9668371687eca6686b28d32c245d1b16897e3
Long Nguyen <lnguyen@globalia.ca>
2020-08-24 18:07:39 -0400

    setup: front-end

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+// var gulpCssPreprocessor = require('gulp-css-preprocessor');
+var header = require('gulp-header');
+var minify = require('gulp-minifier');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+// CSS preprocessor
+gulp.task('styles', function() {
+  return gulp.src('src/css/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(header('/* Generated on: ' + new Date() + ' */\n'))
+    .pipe(gulp.dest('docs/css/'))
+    .pipe(minify({minify: true, minifyCSS: true}))
+    .pipe(header('/* Generated on: ' + new Date() + ' */\n'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('docs/css/'));
+});
+
+
+// WATCH
+gulp.task('watch', gulp.series('styles', (done) => {
+    gulp.watch('src/css/**/*.scss', gulp.parallel('styles'));
+}));
+
+
+// TASKS
+// Bundled tasks
+gulp.task('default', gulp.parallel('styles'));
