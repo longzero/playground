77f2d6f5b8092fc89f3166ccccdcf3c1403c70f0
Long Nguyen <lnguyen@globalia.ca>
2020-11-11 12:58:11 -0500

    devevlopment: placeholder image

--- docs/clubt/js/main.js
+++ docs/clubt/js/main.js
@@ -30,7 +30,7 @@ if (window.location.href.indexOf('.local') > -1) {
   document.addEventListener('error', function (event) {
     if (event.target.tagName.toLowerCase() !== 'img') return;
     // https://townsquare.media/site/442/files/2019/11/baby-yoda-11.jpg?w=1603
-    event.target.src = window.location.origin + '/images/baby-yoda-11.jpg';
+    event.target.src = window.location.origin + '/images/unknown-species.png';
   }, true);
 }
 
