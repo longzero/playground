644296634ebf893d2ccb0e767d1efea683b1c1de
Long Nguyen <lnguyen@globalia.ca>
2020-11-10 21:46:52 -0500

    clubt: setup

---
+++ docs/clubt/js/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const navigation = document.querySelector('.js-site-navigation');
+const navigationToggle = document.querySelector('.js-navigation-toggle');
+
+const navigationLink = document.querySelectorAll('.js-site-navigation > ul > li > a');
+for (let i = 0; i < navigationLink.length; i++) {
+  navigationLink[i].addEventListener('click', function(){
+    if (this.nextSibling) {
+      console.log('has a sibling')
+      document.body.classList.toggle('open-sub-navigation')
+      this.parentNode.classList.toggle('active');
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  })
+}
+
+navigationToggle.addEventListener('click', function(){
+  document.body.classList.toggle('open-navigation')
+})
+
+// --------------------------------------------------------------------------------------------
+// FOR DEVELOPMENT ONLY
+// --------------------------------------------------------------------------------------------
+if (window.location.href.indexOf('.local') > -1) {
+  console.log("readyState: " + document.readyState)
+  console.log('Website is running locally')
+
+  document.addEventListener('error', function (event) {
+    if (event.target.tagName.toLowerCase() !== 'img') return;
+    // https://townsquare.media/site/442/files/2019/11/baby-yoda-11.jpg?w=1603
+    event.target.src = window.location.origin + '/images/baby-yoda-11.jpg';
+  }, true);
+}
+
+console.log("main.js loaded")
